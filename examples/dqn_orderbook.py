e49b76c0f40aacf807e3e0380e122f469e0224c1
Jose Oliveros <jose.oliveros.1983@gmail.com>
2021-05-07 15:44:01 -0500

    restore dqn_atari example
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook.py
+++ examples/dqn_orderbook.py
@@ -1,75 +1,84 @@
+#! /usr/bin/env python
+
 from __future__ import division
-import argparse
-
+from exchange_data.models.resnet.model import Model as ResnetModel
 from PIL import Image
-import numpy as np
-import gym
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
-from tensorflow.keras.optimizers import Adam
-import tensorflow.keras.backend as K
-
 from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
-from rl.memory import SequentialMemory
-from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
+from rl.core import Processor
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from tensorflow.keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import alog
+import argparse
+import gym
+import numpy as np
+import tensorflow.keras.backend as K
+import tgym.envs
 
 
-INPUT_SHAPE = (84, 84)
-WINDOW_LENGTH = 4
 
 
 class AtariProcessor(Processor):
-    def process_observation(self, observation):
-        assert observation.ndim == 3  # (height, width, channel)
-        img = Image.fromarray(observation)
-        img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
-        processed_observation = np.array(img)
-        assert processed_observation.shape == INPUT_SHAPE
-        return processed_observation.astype('uint8')  # saves storage in experience memory
-
-    def process_state_batch(self, batch):
-        # We could perform this processing step in `process_observation`. In this case, however,
-        # we would need to store a `float32` array instead, which is 4x more memory intensive than
-        # an `uint8` array. This matters if we store 1M observations.
-        processed_batch = batch.astype('float32') / 255.
-        return processed_batch
+    # def process_observation(self, observation):
+    #     assert observation.ndim == 3  # (height, width, channel)
+    #     img = Image.fromarray(observation)
+    #     img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
+    #     processed_observation = np.array(img)
+    #     assert processed_observation.shape == INPUT_SHAPE
+    #     return processed_observation.astype('uint8')  # saves storage in experience memory
+    #
+    # def process_state_batch(self, batch):
+    #     # We could perform this processing step in `process_observation`. In this case, however,
+    #     # we would need to store a `float32` array instead, which is 4x more memory intensive than
+    #     # an `uint8` array. This matters if we store 1M observations.
+    #     processed_batch = batch.astype('float32') / 255.
+    #     return processed_batch
 
     def process_reward(self, reward):
         return np.clip(reward, -1., 1.)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', choices=['train', 'test'], default='train')
-parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
+parser.add_argument('--env-name', type=str, default='orderbook-frame-env-v0')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
 # Get the environment and extract the number of actions.
-env = gym.make(args.env_name)
+kwargs = dict(
+    database_name='binance_futures',
+    depth=84,
+    interval='6h',
+    group_by='30s',
+    sequence_length=32,
+    symbol='ETCUSDT',
+    window_size='2m',
+    summary_interval=4,
+    min_change=0.01
+)
+
+WINDOW_LENGTH = 4
+INPUT_SHAPE = (WINDOW_LENGTH, kwargs['sequence_length'], kwargs['depth'] * 2, 1)
+
+env = gym.make(args.env_name, **kwargs)
 np.random.seed(123)
 env.seed(123)
 nb_actions = env.action_space.n
 
 # Next, we build our model. We use the same model that was described by Mnih et al. (2015).
-input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
-model = Sequential()
+input_shape = INPUT_SHAPE
+alog.info(input_shape)
 
-# (width, height, channels)
-model.add(Permute((2, 3, 1), input_shape=input_shape))
+model = ResnetModel(
+    input_shape=input_shape,
+    num_categories=2,
+    include_last=False,
+    batch_size=2,
+    **kwargs
+)
 
-model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, (3, 3), strides=(1, 1)))
-model.add(Activation('relu'))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dense(nb_actions))
-model.add(Activation('linear'))
 print(model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in tensorflow.keras optimizer and
@@ -83,7 +92,7 @@ processor = AtariProcessor()
 # (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
 # so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
+                              nb_steps=10000)
 
 # The trade-off between exploration and exploitation is difficult and an on-going research topic.
 # If you want, you can experiment with the parameters or use a different policy. Another popular one
@@ -91,10 +100,20 @@ policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., valu
 # policy = BoltzmannQPolicy(tau=1.)
 # Feel free to give it a try!
 
-dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-               processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-               train_interval=4, delta_clip=1.)
-dqn.compile(Adam(lr=.00025), metrics=['mae'])
+dqn = DQNAgent(
+    delta_clip=1.,
+    gamma=.99,
+    memory=memory,
+    model=model,
+    nb_actions=nb_actions,
+    nb_steps_warmup=1000,
+    policy=policy,
+    processor=processor,
+    target_model_update=1000,
+    train_interval=12,
+)
+
+dqn.compile(Adam(lr=.0000025), metrics=['mae'])
 
 if args.mode == 'train':
     # Okay, now it's time to learn something! We capture the interrupt exception so that training
@@ -102,15 +121,16 @@ if args.mode == 'train':
     weights_filename = f'dqn_{args.env_name}_weights.h5f'
     checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
     log_filename = f'dqn_{args.env_name}_log.json'
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=10000)]
     callbacks += [FileLogger(log_filename, interval=100)]
-    dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
+    dqn.fit(env, verbose=2, callbacks=callbacks, nb_steps=1750000, log_interval=1000)
 
     # After training is done, we save the final weights one more time.
     dqn.save_weights(weights_filename, overwrite=True)
 
     # Finally, evaluate our algorithm for 10 episodes.
     dqn.test(env, nb_episodes=10, visualize=False)
+
 elif args.mode == 'test':
     weights_filename = f'dqn_{args.env_name}_weights.h5f'
     if args.weights:
