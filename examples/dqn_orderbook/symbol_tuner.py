7d5aff1cb53edc2f4e3f18565aa5ee415fa841a8
Jose Oliveros <jose.oliveros.1983@gmail.com>
2022-01-29 20:54:37 -0600

    tuning
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook/symbol_tuner.py
+++ examples/dqn_orderbook/symbol_tuner.py
@@ -186,12 +186,12 @@ class SymbolTuner(StudyWrapper, Messenger):
         # self._kwargs['depth'] = self.trial.suggest_int('depth', 36, 48)
         # self._kwargs['interval'] = f'{hparams["interval_minutes"] * 60}m'
         # self._kwargs['interval2'] = f'{hparams["interval_minutes2"] * 15}m'
-        self._kwargs['lr'] = self.trial.suggest_float('lr', 1e-8, 1e-5)
+        self._kwargs['lr'] = self.trial.suggest_float('lr', 1e-7, 1e-5)
         # self._kwargs['max_flat_position_length'] = self.trial.suggest_int('max_flat_position_length', 0, 200)
         # self._kwargs['max_negative_pnl'] = self.trial.suggest_float('max_negative_pnl', -20/100, -0.5/100)
         # self._kwargs['max_position_length'] = self.trial.suggest_int('max_position_length', 0, 72)
         # self._kwargs['max_short_position_length'] = self.trial.suggest_int('max_short_position_length', 83, 320)
-        self._kwargs['nb_steps'] = self.trial.suggest_int('nb_steps', int(5e4), int(1e5))
+        # self._kwargs['nb_steps'] = self.trial.suggest_int('nb_steps', int(5e4), int(1e5))
         # self._kwargs['nb_steps_2'] = self.trial.suggest_int('nb_steps_2', 1000, int(5e4))
         # self._kwargs['num_conv'] = self.trial.suggest_int('num_conv', 3, 5)
         # self._kwargs['round_decimals'] = self.trial.suggest_int('round_decimals', 4, 5)
@@ -204,8 +204,8 @@ class SymbolTuner(StudyWrapper, Messenger):
         # self._kwargs['target_model_update'] = self.trial.suggest_int('target_model_update', 18, 84)
         # self._kwargs['gap_enabled'] = self.trial.suggest_categorical('gap_enabled', [True, False])
 
-        # self._kwargs['max_change'] = self.trial.suggest_float('max_change', 0.001, 0.01)
-        # self._kwargs['min_flat_change'] = self.trial.suggest_float('min_flat_change', -0.01, 0.0)
+        self._kwargs['max_change'] = self.trial.suggest_float('max_change', 0.001, 0.02)
+        self._kwargs['min_flat_change'] = self.trial.suggest_float('min_flat_change', -0.01, 0.0)
 
         self._kwargs['max_flat_position_length'] = 44
         self._kwargs['max_position_length'] = 31
