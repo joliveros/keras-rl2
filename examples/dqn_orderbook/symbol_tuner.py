0c431cc4ba3f4939342f3c9fb4eeee0a6de00c47
Jose Oliveros <jose.oliveros.1983@gmail.com>
2022-06-10 21:03:57 -0600

    tune others
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook/symbol_tuner.py
+++ examples/dqn_orderbook/symbol_tuner.py
@@ -176,7 +176,7 @@ class SymbolTuner(StudyWrapper):
                 # dense_width=self.trial.suggest_int('dense_width', 4, 64),
                 # block_filter_factor=self.trial.suggest_int('block_filter_factor', 1, 10),
                 # block_kernel=self.trial.suggest_int('block_kernel', 1, 8),
-                # interval_minutes=self.trial.suggest_int('interval_minutes', 4, 24),
+                interval_minutes=self.trial.suggest_int('interval_minutes', 4, 24),
                 # interval_minutes2=self.trial.suggest_int('interval_minutes2', 4, 4 * 6),
                 # kernel_size=self.trial.suggest_categorical('kernel_size', [2, 3, 4]),
                 # dense_size=self.trial.suggest_int('dense_size', 256, 512),
@@ -184,16 +184,16 @@ class SymbolTuner(StudyWrapper):
                 # max_pooling_strides=self.trial.suggest_int('max_pooling_strides', 1, 16),
                 # padding=self.trial.suggest_int('padding', 1, 8),
                 # strides=self.trial.suggest_int('strides', 1, 16),
-                # eps_greedy_policy_steps=self.trial.suggest_int('eps_greedy_policy_steps', 1000, 34000)
+                eps_greedy_policy_steps=self.trial.suggest_int('eps_greedy_policy_steps', 1000, 34000)
             )
 
             # self._kwargs['fee_ratio'] = self.trial.suggest_float('fee_ratio', 0.9, 2.0)
             # self._kwargs['trading_fee'] = self.trial.suggest_float('trading_fee', 0.0004, 0.01)
             # self._kwargs['policy_value_max'] = self.trial.suggest_float('policy_value_max', 0.001, 0.9)
             # self._kwargs['batch_size'] = self.trial.suggest_int('batch_size', 32, 128)
-            # self._kwargs['lr'] = self.trial.suggest_float('lr', 1e-04, 0.01)
+            self._kwargs['lr'] = self.trial.suggest_float('lr', 1e-04, 0.01)
             # self._kwargs['depth'] = self.trial.suggest_int('depth', 12, 36)
-            # self._kwargs['interval'] = f'{hparams["interval_minutes"] * 60}m'
+            self._kwargs['interval'] = f'{hparams["interval_minutes"] * 60}m'
             # self._kwargs['interval2'] = f'{hparams["interval_minutes2"] * 15}m'
             # self._kwargs['max_flat_position_length'] = self.trial.suggest_int('max_flat_position_length', 55, 300)
             # self._kwargs['max_negative_pnl'] = self.trial.suggest_float('max_negative_pnl', -20/100, -0.5/100)
@@ -205,7 +205,7 @@ class SymbolTuner(StudyWrapper):
             # self._kwargs['round_decimals'] = self.trial.suggest_int('round_decimals', 4, 5)
             # self._kwargs['sequence_length'] = self.trial.suggest_int('sequence_length', 12, 21)
             # self._kwargs['train_recent_data'] = self.trial.suggest_categorical('train_recent_data', [True, False])
-            self._kwargs['window_length'] = self.trial.suggest_int('window_length', 1, 4)
+            # self._kwargs['window_length'] = self.trial.suggest_int('window_length', 1, 4)
             # self._kwargs['min_change'] = self.trial.suggest_float('min_change', 0.0, 0.02)
             # self._kwargs['cache_limit'] = self.trial.suggest_int('cache_limit', 700, 5000)
             # self._kwargs['train_interval'] = self.trial.suggest_int('train_interval', 26, 78)
