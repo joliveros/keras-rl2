05bf274b4d24ceab11351c49fcde732f3da534da
Jose Oliveros <jose.oliveros.1983@gmail.com>
2022-11-07 15:51:38 -0700

    reduce num trades
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook/symbol_tuner.py
+++ examples/dqn_orderbook/symbol_tuner.py
@@ -208,12 +208,12 @@ class SymbolTuner(StudyWrapper):
             # self._kwargs['offset_interval'] = f'{hparams["_offset_interval"] * 60}m'
             # self._kwargs['interval'] = f'{hparams["interval_minutes"] * 60}m'
             # self._kwargs['interval2'] = f'{hparams["interval_minutes2"] * 15}m'
-            # self._kwargs['max_flat_position_length'] \
-            # = self.trial.suggest_int('max_flat_position_length', 1, 200)
+            self._kwargs['max_flat_position_length'] \
+                 = self.trial.suggest_int('max_flat_position_length', 1, 200)
             # self._kwargs['max_negative_pnl'] = self.trial.suggest_float('max_negative_pnl', -20/100, -0.5/100)
             # self._kwargs['max_position_length'] = self.trial.suggest_int('max_position_length', 0, 72)
-            # self._kwargs['max_short_position_length'] \
-                # = self.trial.suggest_int('max_short_position_length', 1, 200)
+            self._kwargs['max_short_position_length'] \
+                = self.trial.suggest_int('max_short_position_length', 1, 200)
             self._kwargs['nb_steps'] = self.trial.suggest_int('nb_steps', 5000, 100000, log=True)
             # self._kwargs['nb_steps_2'] = self.trial.suggest_int('nb_steps_2', 1000, int(5e4))
             self._kwargs['num_conv'] = self.trial.suggest_int('num_conv', 1, 15)
