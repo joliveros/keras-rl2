3aaee18f04b3c70a5d0136d81e39ffff01afbcf3
Jose Oliveros <jose.oliveros.1983@gmail.com>
2022-12-18 19:18:17 -0600

    exclude trades
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook/symbol_agent.py
+++ examples/dqn_orderbook/symbol_agent.py
@@ -182,32 +182,26 @@ class SymbolAgent(object):
         # nb_steps_avg = sum(nb_steps) / len(nb_steps)
 
         capital = [info['capital'] / info['num_steps'] for info in history.history['info']]
-        trades = []
-        trades += [info['trades'] for info in history.history['info']]
 
         def flatten(l):
             return [item for sublist in l for item in sublist]
 
-        trades = flatten(trades)
-        trades = list({hash(t): t for t in trades}.values())
-
         capital_avg = sum(capital) / len(capital)
-        pos_trades = len([t.pnl for t in trades if t.pnl > 0])
-
-        trade_len = len(trades)
-        alog.info(dict(pos_trades=pos_trades, trade_len=trade_len))
-        trade_ratio = pos_trades / trade_len
-
-        if trade_ratio != 0:
-            trade_ratio = math.log((trade_ratio) ** (1/2))
-
-        self.study.set_user_attr('trades', len(trades))
         self.study.set_user_attr('capital', capital_avg)
 
-
-        capital_ratio = (1 - self.trade_ratio)
-
-        pos_trades = (pos_trades **(1/24)) * 0.1
+        # trades = []
+        # trades += [info['trades'] for info in history.history['info']]
+        # trades = flatten(trades)
+        # trades = list({hash(t): t for t in trades}.values())
+        # pos_trades = len([t.pnl for t in trades if t.pnl > 0])
+        # trade_len = len(trades)
+        # alog.info(dict(pos_trades=pos_trades, trade_len=trade_len))
+        # trade_ratio = pos_trades / trade_len
+        # if trade_ratio != 0:
+        #     trade_ratio = math.log((trade_ratio) ** (1/2))
+        # self.study.set_user_attr('trades', len(trades))
+        # capital_ratio = (1 - self.trade_ratio)
+        # pos_trades = (pos_trades **(1/24)) * 0.1
 
         return capital_avg
 
