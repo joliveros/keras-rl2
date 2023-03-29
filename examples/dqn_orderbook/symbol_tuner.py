41a3e385640cd5df5caa5426c5a6e5b35a110d78
Jose Oliveros <jose.oliveros.1983@gmail.com>
2023-03-29 17:48:52 -0600

    tune max-pooling
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook/symbol_tuner.py
+++ examples/dqn_orderbook/symbol_tuner.py
@@ -186,7 +186,7 @@ class SymbolTuner(StudyWrapper):
                 # interval_minutes=self.trial.suggest_int('interval_minutes', 1, 24 * 7),
                 # interval_minutes2=self.trial.suggest_int('interval_minutes2', 4, 4 * 6),
                 # kernel_size=self.trial.suggest_int('kernel_size', 1, 17),
-                # max_pooling_kernel=self.trial.suggest_int('max_pooling_kernel', 1, 21),
+                max_pooling_kernel=self.trial.suggest_int('max_pooling_kernel', 1, 16),
                 max_pooling_strides=self.trial.suggest_int('max_pooling_strides', 1, 16),
                 # padding=self.trial.suggest_int('padding', 1, 8),
                 # strides=self.trial.suggest_int('strides', 1, 36),
