dd987437d92a880183c994a6d5cc51df19b8206c
Jose Oliveros <jose.oliveros.1983@gmail.com>
2023-05-13 17:36:16 -0600

    reduce num layers
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- examples/dqn_orderbook/symbol_tuner.py
+++ examples/dqn_orderbook/symbol_tuner.py
@@ -235,7 +235,7 @@ class SymbolTuner(StudyWrapper):
             def conv_layer(layer_name):
                 return self.trial.suggest_categorical(layer_name, [None, 'conv', 'identity'])
 
-            num_layers = 13
+            num_layers = 5
             
             for layer_index in range(num_layers):
                  name = f'conv_layer_{layer_index}'
